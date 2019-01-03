38520e353c0cdb4d7c2beb1edb1e921686463ed4
王章仲 <zhangzhong.wang@yingxiong.com>
2019-01-03 20:02:20 +0800

    nsDecimalNumber分类添加注释

--- JOWCategory/Classes/Foundation+ZZCategory/NSDecimalNumber+ZZ.h
+++ JOWCategory/Classes/Foundation+ZZCategory/NSDecimalNumber+ZZ.h
@@ -18,71 +18,115 @@
 
 @interface NSDecimalNumber (Display)
 
+/**
+ * 固定小数位数的十进制数
+ */
 - (NSDecimalNumber *)zz_formatWithScale:(NSUInteger)decimalDigits;
+
+/**
+ * 固定小数位数的十进制数字符串
+ */
 - (NSString *)zz_displayAmountWithScale:(NSUInteger)decimalDigits;
+
+/**
+ * 十进制数字符串
+ *
+ * @param maximumDecimalDigits 小数位最大有效位数
+ * @param minimumDecimalDigits 小数位最小有效位数
+ */
 - (NSString *)zz_displayAmountWithScale:(NSUInteger)maximumDecimalDigits
                          effectiveScale:(NSUInteger)minimumDecimalDigits;
+
+/**
+ * 带货币符号的金额显示
+ */
+- (NSString *)zz_localCurrencyWithScale:(NSUInteger)decimalDigits;
+
 @end
 
 
 @interface NSDecimalNumber (Calculate)
 
+// MARK: - 和
 /**
  * 求和
- 
+ *
  * @param numbers 加数数组
  */
 + (NSDecimalNumber *)zz_sumWithDecimalNumberArray:(NSArray<NSDecimalNumber *> *)numbers;
 
 /**
  * 求和
- 
+ *
  * @param numberA 加数A
  * @param numberB 加数B
  */
 + (NSDecimalNumber *)zz_sumWithDecimalNumberA:(NSDecimalNumber *)numberA
                                DecimalNumberB:(NSDecimalNumber *)numberB;
 
+/**
+ * 加
+ *
+ * @param number 加数
+ */
 - (NSDecimalNumber *)zz_decimalNumberByAdding:(NSDecimalNumber *)number;
 
+// MARK: - 差
 /**
  * 求差
- 
+ *
  * @param subtracted 减数
  * @param subtract   被减数
  */
 + (NSDecimalNumber *)zz_differenceWithSubtractedDecimalNumber:(NSDecimalNumber *)subtracted
                                         subtractDecimalNumber:(NSDecimalNumber *)subtract;
 
+/**
+ * 减
+ *
+ * @param number 减数
+ */
 - (NSDecimalNumber *)zz_decimalNumberBySubtracting:(NSDecimalNumber *)number;
 
+// MARK: - 积
 /**
  * 乘积
- 
+ *
  * @param multipliers 乘数数组
  */
 + (NSDecimalNumber *)zz_productWithMultiplierDecimalNumberArray:(NSArray<NSDecimalNumber *> *)multipliers;
 
 /**
  * 乘积
-
+ *
  * @param multiplierA 乘数A
  * @param multiplierB 乘数B
  */
 + (NSDecimalNumber *)zz_productWithMultiplierDecimalNumberA:(NSDecimalNumber *)multiplierA
                                    multiplierDecimalNumberB:(NSDecimalNumber *)multiplierB;
 
+/**
+ * 乘
+ *
+ * @param number 乘数
+ */
 - (NSDecimalNumber *)zz_decimalNumberByMultiplyingBy:(NSDecimalNumber *)number;
 
+// MARK: - 商
 /**
  * 求商
- 
- * @param divisorA 除数A
- * @param divisorB 被除数B
+ *
+ * @param divisorA 被除数A
+ * @param divisorB 除数B
  */
 + (NSDecimalNumber *)zz_quotientWithDivisorDecimalNumberA:(NSDecimalNumber *)divisorA
                                     divisorDecimalNumberB:(NSDecimalNumber *)divisorB;
 
+/**
+ * 除
+ *
+ * @param number 除数
+ */
 - (NSDecimalNumber *)zz_decimalNumberByDividingBy:(NSDecimalNumber *)number;
 
 
--- JOWCategory/Classes/Foundation+ZZCategory/NSDecimalNumber+ZZ.m
+++ JOWCategory/Classes/Foundation+ZZCategory/NSDecimalNumber+ZZ.m
@@ -14,15 +14,28 @@
 @end
 
 @implementation NSDecimalNumber (Display)
+
+/**
+ * 固定小数位数的十进制数
+ */
 - (NSDecimalNumber *)zz_formatWithScale:(NSUInteger)decimalDigits {
     NSDecimalNumberHandler *handle = [NSDecimalNumberHandler decimalNumberHandlerWithRoundingMode:NSRoundBankers scale:decimalDigits raiseOnExactness:YES raiseOnOverflow:NO raiseOnUnderflow:NO raiseOnDivideByZero:YES];
     return [self decimalNumberByRoundingAccordingToBehavior:handle];
 }
 
+/**
+ * 固定小数位数的十进制数字符串
+ */
 - (NSString *)zz_displayAmountWithScale:(NSUInteger)decimalDigits {
     return [self zz_displayAmountWithScale:decimalDigits effectiveScale:decimalDigits];
 }
 
+/**
+ * 十进制数字符串
+ *
+ * @param maximumDecimalDigits 小数位最大有效位数
+ * @param minimumDecimalDigits 小数位最小有效位数
+ */
 - (NSString *)zz_displayAmountWithScale:(NSUInteger)maximumDecimalDigits
                          effectiveScale:(NSUInteger)minimumDecimalDigits
 {
@@ -41,14 +54,35 @@
     return display;
 }
 
+/**
+ * 带货币符号的金额显示
+ */
+- (NSString *)zz_localCurrencyWithScale:(NSUInteger)decimalDigits {
+    NSString *display = [[self zz_formatWithScale:2] stringValue];
+    
+    NSNumberFormatter *numberFormatter = [[NSNumberFormatter alloc] init];
+    // 设置格式
+    //    numberFormatter.locale = [NSLocale localeWithLocaleIdentifier:@"en_CN"];
+    numberFormatter.numberStyle = kCFNumberFormatterCurrencyStyle;
+    numberFormatter.decimalSeparator = K_ZZ_DecimalSeparator;
+    numberFormatter.minimumIntegerDigits = 1;  // 最小整数位数
+    numberFormatter.maximumFractionDigits = decimalDigits;  // 最大小数位数
+    numberFormatter.minimumFractionDigits = decimalDigits;  // 最小小数位数
+    
+    display = [numberFormatter stringFromNumber:[NSNumber numberWithDouble:[display doubleValue]]];
+    
+    return display;
+}
+
 @end
 
 
 @implementation NSDecimalNumber (Calculate)
 
+// MARK: - 和
 /**
  * 求和
- 
+ *
  * @param numbers 加数数组
  */
 + (NSDecimalNumber *)zz_sumWithDecimalNumberArray:(NSArray<NSDecimalNumber *> *)numbers
@@ -75,7 +109,7 @@
 
 /**
  * 求和
- 
+ *
  * @param numberA 加数A
  * @param numberB 加数B
  */
@@ -85,6 +119,11 @@
     return [numberA zz_decimalNumberByAdding:numberB];
 }
 
+/**
+ * 加
+ *
+ * @param number 加数
+ */
 - (NSDecimalNumber *)zz_decimalNumberByAdding:(NSDecimalNumber *)number
 {
     NSDecimalNumber *num1 = nil;
@@ -107,9 +146,10 @@
     return num;
 }
 
+// MARK: - 差
 /**
  * 求差
- 
+ *
  * @param subtracted 减数
  * @param subtract   被减数
  */
@@ -120,6 +160,11 @@
     return [subtracted zz_decimalNumberBySubtracting:subtract];
 }
 
+/**
+ * 减
+ *
+ * @param number 减数
+ */
 - (NSDecimalNumber *)zz_decimalNumberBySubtracting:(NSDecimalNumber *)number
 {
     NSDecimalNumber *num1 = nil;
@@ -142,9 +187,10 @@
     return num;
 }
 
+// MARK: - 积
 /**
  * 乘积
- 
+ *
  * @param multipliers 乘数数组
  */
 + (NSDecimalNumber *)zz_productWithMultiplierDecimalNumberArray:(NSArray<NSDecimalNumber *> *)multipliers
@@ -171,7 +217,7 @@
 
 /**
  * 乘积
- 
+ *
  * @param multiplierA 乘数A
  * @param multiplierB 乘数B
  */
@@ -181,6 +227,11 @@
     return [multiplierA zz_decimalNumberByMultiplyingBy:multiplierB];
 }
 
+/**
+ * 乘
+ *
+ * @param number 乘数
+ */
 - (NSDecimalNumber *)zz_decimalNumberByMultiplyingBy:(NSDecimalNumber *)number
 {
     NSDecimalNumber *num1 = nil;
@@ -203,11 +254,12 @@
     return num;
 }
 
+// MARK: - 商
 /**
  * 求商
- 
- * @param divisorA 除数A
- * @param divisorB 被除数B
+ *
+ * @param divisorA 被除数A
+ * @param divisorB 除数B
  */
 + (NSDecimalNumber *)zz_quotientWithDivisorDecimalNumberA:(NSDecimalNumber *)divisorA
                                     divisorDecimalNumberB:(NSDecimalNumber *)divisorB
@@ -215,6 +267,11 @@
     return [divisorA zz_decimalNumberByDividingBy:divisorB];
 }
 
+/**
+ * 除
+ *
+ * @param number 除数
+ */
 - (NSDecimalNumber *)zz_decimalNumberByDividingBy:(NSDecimalNumber *)number
 {
     NSDecimalNumber *num1 = nil;
