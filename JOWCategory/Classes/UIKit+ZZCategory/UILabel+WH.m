19521243c7ea758564ffb0026a59d63b347cf6e0
王章仲 <zhangzhong.wang@yingxiong.com>
2019-02-25 17:57:42 +0800

    修改

--- JOWCategory/Classes/UIKit+ZZCategory/UILabel+WH.m
+++ JOWCategory/Classes/UIKit+ZZCategory/UILabel+WH.m
@@ -7,7 +7,11 @@
 //
 
 #import "UILabel+WH.h"
-#import "../Foundation+ZZCategory/NSString+ZZ.h"
+
+#define IS_NULL(x)          (!x || [x isKindOfClass:[NSNull class]])
+#define IS_EMPTY_STRING(x)  (IS_NULL(x) || [x isEqual:@""] || [x isEqual:@"(null)"])
+#define DEFUSE_EMPTY_STRING(x)     (!IS_EMPTY_STRING(x) ? x : @"")
+#define DISPLAY_STRING(text, defaultStr) ([DEFUSE_EMPTY_STRING(text) isEqualToString:@""] ? ([DEFUSE_EMPTY_STRING(defaultStr) isEqualToString:@""] ? @"" : DEFUSE_EMPTY_STRING(defaultStr)) : DEFUSE_EMPTY_STRING(text))
 
 @implementation UILabel (WH)
 
@@ -101,13 +105,100 @@
 - (void)zz_setTextNumber:(NSString *)sender {
     __weak typeof(self) weakself = self;
     
-    [NSString zz_setNumberFrom:self.text to:sender change:^(NSString *num) {
+    [self zz_setNumberFrom:self.text to:sender durationTime:1.0 change:^(NSString *num) {
         weakself.text = num;
     }];
 }
 
 
-
+- (void)zz_setNumberFrom:(NSString *)from to:(NSString *)to durationTime:(float)time change:(void (^) (NSString *num))change {
+    
+    if (!to && from) {
+        !change ? : change(from);
+        return;
+    }
+    
+    if (!from && !to) {
+        !change ? : change(@"0.00");
+        return;
+    }
+    
+    //    if (!DEFUSE_EMPTY_STRING(from) || !DEFUSE_EMPTY_STRING(to) || !from || !to) {
+    //        !change ? : change(@"0.00");
+    //        return;
+    //    }
+    
+    if ([from isEqualToString:to]) {
+        !change ? : change(to);
+        return;
+    }
+    
+    //动画持续时间
+    float durationTime = time;
+    
+    //旧值
+    CGFloat old = [DEFUSE_EMPTY_STRING(from) doubleValue];
+    
+    //新值
+    CGFloat new = [DEFUSE_EMPTY_STRING(to) doubleValue];
+    
+    //新旧值 差
+    CGFloat poor = new - old;
+    
+    if (poor == 0) {
+        !change ? : change(!from ? to : from);
+        return;
+    }
+    
+    //每帧增加数值
+    CGFloat ratio = poor / (60.0 * durationTime);
+    
+    //计算保留小数点后位数
+    NSUInteger scale = 0;
+    
+    BOOL isNumber = [to zz_validateStringWithFormat:@"0123456789."];
+    BOOL partCount = [to componentsSeparatedByString:@"."].count == 2;
+    
+    if ((isNumber && partCount)) {
+        scale = [[to componentsSeparatedByString:@"."].lastObject length];
+    }
+    
+    //根据保留小数位数，定义字符显示格式
+    NSString *format = [NSString stringWithFormat:@"%@.%@f", @"%", @(scale)];
+    
+    
+    
+    __block CGFloat resValue = [from doubleValue];
+    
+    NSTimeInterval period = 0.017; //设置时间间隔
+    
+    dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+    
+    dispatch_source_t _timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, queue);
+    
+    dispatch_source_set_timer(_timer, dispatch_walltime(NULL, 0), period * NSEC_PER_SEC, 0); //按照时间间隔执行
+    
+    dispatch_source_set_event_handler(_timer, ^{
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            
+            resValue = resValue + ratio;
+            
+            if (((resValue >= new) && (ratio >= 0)) || ((resValue <= new) && (ratio <= 0))) {
+                dispatch_source_cancel(_timer);
+                NSString *displayString = [NSString stringWithFormat:format, new];
+                !change ? : change(displayString);
+            } else {
+                NSString *displayString = [NSString stringWithFormat:format, resValue];
+                !change ? : change(displayString);
+            }
+        });
+        
+    });
+    
+    dispatch_resume(_timer);
+    
+}
 
 @end
 
