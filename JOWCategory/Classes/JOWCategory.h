bbab6953fe7fd6fc65a1fdb8bc813bc954ed89aa
王章仲 <zhangzhong.wang@yingxiong.com>
2019-02-26 11:16:21 +0800

    头文件修改

--- JOWCategory/Classes/JOWCategory.h
+++ JOWCategory/Classes/JOWCategory.h
@@ -8,19 +8,21 @@
 #ifndef JOWCategory_h
 #define JOWCategory_h
 
-#import "NSArray+ZZ.h"
-#import "NSDate+ZZ.h"
-#import "NSDecimalNumber+ZZ.h"
-#import "NSDictionary+ZZ.h"
-#import "NSString+ZZ.h"
-#import "ZZFoundationHeader.h"
-#import "UIButton+Countdown.h"
-#import "UIButtonTextImage.h"
-#import "UIColor+Gradient.h"
-#import "UIImage+QRCode.h"
-#import "UILabel+WH.h"
-#import "UIView+Frame.h"
-#import "UIView+Gradient.h"
+//#import "NSArray+ZZ.h"
+//#import "NSDate+ZZ.h"
+//#import "NSDecimalNumber+ZZ.h"
+//#import "NSDictionary+ZZ.h"
+//#import "NSString+ZZ.h"
+//#import "ZZFoundationHeader.h"
+//#import "UIButton+Countdown.h"
+//#import "UIButtonTextImage.h"
+//#import "UIColor+Gradient.h"
+//#import "UILabel+WH.h"
+//#import "UIView+Frame.h"
+//#import "UIView+Gradient.h"
 
+#import "JOWFoundation.h"
+#import "JOWUIKit.h"
+#import "UIImage+QRCode.h"
 
 #endif /* JOWCategory_h */
